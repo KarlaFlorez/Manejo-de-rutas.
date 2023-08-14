a373b9de7cfd11c5ab50280587d5fa0dba490dea
Karla Florez <114744321+KarlaFlorez@users.noreply.github.com>
2023-08-14 16:37:57 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+"""
+De flask importamos el módulo Flask, también trabajaremos con archivos Json, por lo tanto también importamos 
+el método jsonify
+"""
+from flask import Flask, jsonify
+from products import products #Importamos de products el método proucts.
+app = Flask(__name__)
+
+#Se crea la ruta para nuestro servidor.
+@app.route('/ping')#Va a tener un método GET.
+
+def ping(): #Función para comprar el funcionamienro.
+    return jsonify({"message": "pong!"}) #Se envía un objeto.
+
+
+@app.route('/products')#Ruta productos, se mostrará la lista de productos.
+def getProcucts():
+    #Se envía un objeto, el cual, tiene la propiedad products, y su valor es
+    #la lista de3 productos.
+    return jsonify({"products": products, "message": "Product's List"}) 
+
+#En caso de querer llamar un solo nombre de la lista de productos, creamos una nueva ruta al igual que una nueva función.
+@app.route('/products/<string:product_name>')
+
+def getProduct(product_name):
+    #Recorre el método products, para encontrar el valor ingresado por los clientes,
+    #por medio de un ciclo For.
+    productsFound = [product for product in products if product['name'] == product_name]
+    #Ahora creamos una condición, la cuál nos ayudará a definir si el valor ingresado el mayor a cero.
+    #Se utilizará una función, Len, esta se encarga de darnos la lóngitud de un arreglo, en este caso nos dará
+    #la lóngitud de la lista.
+    if (len(productsFound) > 0):
+        return jsonify({"product": productsFound[0]}) #En el caso de que el producto sea mayor a cero, nos retornará el primer valor que encuentre.
+    return jsonify({"message": "Product no found."})#Si no encuentra nada, se retornará un mensaje avisando.)
+
+#----------------------------- CREATE PRODUCTS -----------------------------
+if __name__ == '__main__': #Se crea una condición, de que si el archivo se esta ejecutando como principal.
+    app.run(debug=True, port=4000)
---
+++ products.py
@@ -0,0 +1,13 @@
+"""
+Cada que se trabaja con py, se utiliza una base de datos.
+Se crea una variable que tendrá una lista de datos.
+A cada objeto se le da un nombre.
+"""
+products = [
+    {"name": "laptop", "price": 800, "quantity": 4},
+    {"name": "mouse", "price": 40, "quantity": 10},
+    {"name": "monitor", "price": 400, "quantity": 3}
+]
+
+
+
